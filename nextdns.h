fb3c41f24f3f29ddbc0b8cc8189887119e2d5cad
Matthew Carlson <macarl@microsoft.com>
2020-05-13 13:53:44 -0700

    accidently uploaded nextdns.cpp

--- nextdns.h
+++ nextdns.h
@@ -1,5 +1,6 @@
 #ifndef NEXTDNS_H
 #define NEXTDNS_H
+// Configuration is stored in nextdns.cpp and it has not been uploaded since I don't want to share my API key
 // Talks to NextDNS.io and turns on Parental controls
 void NextDNSTurnOnParentalControls();
 // Talks to NextDNS.io and turns on Parental controls
